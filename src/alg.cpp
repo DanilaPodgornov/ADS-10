8582b1d54b6e0334ac3907ba738a077560154c2b
DanilaPodgornov <116427593+DanilaPodgornov@users.noreply.github.com>
2023-06-04 23:11:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,22 +1,15 @@
 // Copyright 2022 NNTU-CS
 #include  <iostream>
-#include  <vector>
 #include  <fstream>
 #include  <locale>
 #include  <cstdlib>
 #include  "tree.h"
 
-int Fact(int n) {
-        int result = 1;
-        for (int i = 1; i <= n; i++) {
-            result *= i;
-        }
-        return result;
-    }
-
 std::vector<char> getPerm(const Tree& tree, int n) {
-    std::vector<char> out;
-    out = tree.getPermutation(tree.root, n, tree.numberOfElements,
-                              std::vector<char> {});
-    return out;
+    std::vector<std::vector<char>> temp = tree.getPermutations();
+    if (temp.size() >= n) {
+        return temp[n - 1];
+    } else {
+        return {};
+    }
 }
